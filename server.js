0a779e85e90a705b00c4317d614e790d57943ccc
JaredWeav <jared.weaver2@gmail.com>
2021-04-28 13:48:01 -0400

    added server

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+  }
+  
+  // Define API routes here
+  
+  // Send every other request to the React app
+  // Define any API routes before this runs
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  });
+
+app.listen(PORT, () => {
+    console.log(`App listening on ${PORT}`)
+})
